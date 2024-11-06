f0babc52a5179b6448a6c72e2cb7ea9a8fa43f05
Mughees110 <zaidimughees@gmail.com>
2024-11-06 16:17:41 +0500

    dsf

--- index.js
+++ index.js
@@ -897,9 +897,9 @@ app.post(
       if (!id) {
         return res.status(400).json({ error: "id is required" });
       }
-      const { name, password } = req.body;
+      const { name, password, email } = req.body;
       // Find the service by ID
-      const user = await User.findById(id);
+      const user = await User.findOne({ email });
 
       if (!user) {
         return res.status(404).json({ error: "User not found" });
